4280f3ee3da2c8a01cbf415d72c4047fdd07a1f4
lei.zhou <1912956608@qq.com>
2020-10-03 16:42:34 +0800

    cheerio解析html及xml文件爬取数据

---
+++ cheerio/index.js
@@ -0,0 +1,65 @@
+const cheerio = require("cheerio"),
+  axios = require("axios"),
+  fs = require("fs"),
+  path = require("path");
+let parentLink = [],
+  indexHTMLUrl = "https://www.doutula.com/article/list/?page=",
+  allPageNum = (async function () {
+    let pageInfo = await axios.get(`${indexHTMLUrl}1`);
+    let $ = cheerio.load(pageInfo.data),
+      btnPagiinationLength = $(".pagination li").length;
+
+    return $(".pagination li")
+      .eq(btnPagiinationLength - 1)
+      .text();
+  })();
+console.log(allPageNum);
+// 初始化
+(function () {
+  for (let i = 1; i <= allPageNum; i++) {
+    getPageInfo(`${indexHTMLUrl}${i}`);
+  }
+})();
+// cheerio用来解析html或者xml文档
+async function getPageInfo(url) {
+  axios.get(url).then((res) => {
+    // 将获取到的数据放到cheerio中进行解析
+    let $ = cheerio.load(res.data),
+      reg = /(.*?)\d/gis;
+    $("#home .col-sm-9>a").each((i, item) => {
+      // 获取到所有的标签头url连接
+      let parentUrl = $(item).attr("href"),
+        parentTitle = $(item).find(".random_title").text();
+      // 全部分类
+      parentLink.push({ url: parentUrl, title: parentTitle });
+    });
+    // console.log(parentLink);
+    getImgInfo(parentLink);
+  });
+}
+async function getImgInfo(parentLink) {
+  // console.log(parentLink);
+  parentLink.forEach((element) => {
+    let res = axios.get(element.url);
+    console.log(res);
+    $ = cheerio.load(res);
+    $(".pic-content img").each((i, item) => {
+      console.log($(item).attr("src"));
+      let imgUrl = $(item).attr("src");
+      let extname = path.extname(imgUrl);
+      let ws = fs.createWriteStream(
+        `./img/${item.title}/${item.title}-${i}${extname}`
+      );
+      // 获取到详细信息之后写入图片
+      sxios.get(
+        imgUrl,
+        { responseType: "stream" }.then((res) => {
+          res.data.pipe(ws);
+          res.data.on("close", (_) => {
+            ws.close();
+          });
+        })
+      );
+    });
+  });
+}
