43a22a14b2459997281682280974e7586fa28711
zhoulei <1912956608@qq.com>
2020-11-01 18:07:22 +0800

    表单进行post请求需要设置urlencoded

--- express/根据货物id动态获取货物info渲染ejs/app.js
+++ express/根据货物id动态获取货物info渲染ejs/app.js
@@ -13,13 +13,18 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-// 首页
+
+// 解析post提交的数据需要用到下面语句进行解析
+// app.use是中间件
+app.use(express.urlencoded());
+
 app.get("/", (req, res) => {
   console.log(122);
   console.log(req.params);
   let options = {};
   res.render("index", options);
 });
+// form get 请求
 app.get("/search", async (req, res) => {
   console.log(req.url);
   // 接受表单发送的请求并拆解成对象的方式输出
@@ -41,5 +46,31 @@ app.get("/search", async (req, res) => {
 });
 app.get("/ajax", (req, res) => {
   res.send("AJAX");
+});
+
+// form post 请求
+app.post("/search", (req, res) => {
+  // post提交的数据不在query上 而且需要 app.use(express.urlencoded());进行解析参数
+  console.log(req.query);
+  console.log(req.body);
+  res.send("post请求");
+});
+// 处理登录
+// 登录页
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  // 查询数据库是否有此用户名和密码
+  let strSql = "select * from user where username = ? and password = ?";
+  let arr = [username, password];
+  let searchResult = await sqlQuery(strSql);
+  if (searchResult.length) {
+    res.send("登录成功");
+  } else {
+    res.send("登录失败,请检查用户名或者密码");
+  }
 });
 module.exports = app;
